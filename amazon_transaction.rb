0aa7ddb85d8224923893aa1a2fd399ab0a5a4d89
monineath <monineathh@gmail.com>
2018-07-16 13:20:44 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -10,9 +10,9 @@ class AmazonTransaction
   end 
   
   def add_item(title, price, quantity = 1)
-    @total += price
+    @total += price * quantity
     @items = ["eggs"]
-    @quantity *= quantity
+  
    
   
     
